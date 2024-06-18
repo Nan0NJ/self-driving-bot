4a619f8b858a990e60b4c844df54fa999d15276a
Nenad Jakovchevski <89221061@student.upr.si>
2024-06-18 20:31:47 +0200

    Grid Layout Added v1.2
    Basic overview of the dimension with setting classes:
    - Controls
    - Car
    - Main
     FOR LATER DEVELOPMENT

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+import Car from "./modules/car";
+
+const canvas : HTMLCanvasElement = document.getElementById('VechicleCanvas') as HTMLCanvasElement;
+
+canvas.width = 300;
+
+const ctx : CanvasRenderingContext2D = canvas.getContext('2d');
+
+const car = new Car(100, 100, 30, 50);
+
+animate();
+
+function animate() {
+    car.update();
+
+    canvas.height = window.innerHeight;
+
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
+
