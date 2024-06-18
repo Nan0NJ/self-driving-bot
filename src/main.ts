2761ea47f126df85df61840f5ba7005e8ce67a91
Nenad Jakovchevski <89221061@student.upr.si>
2024-06-18 20:47:55 +0200

    Built RoadMap v1.3
    Added files:
    - Utills
    - Road

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Car from "./modules/car";
+import Road from "./modules/road";
 
 const canvas : HTMLCanvasElement = document.getElementById('VechicleCanvas') as HTMLCanvasElement;
 
@@ -6,16 +7,25 @@ canvas.width = 300;
 
 const ctx : CanvasRenderingContext2D = canvas.getContext('2d');
 
-const car = new Car(100, 100, 30, 50);
+// Creation of Init Road Object
+const road = new Road(canvas.width / 2, canvas.width * .94);
+// Creation of Init Car Object - to test
+const bestCar = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
 function animate() {
-    car.update();
+    bestCar.update();
 
     canvas.height = window.innerHeight;
 
-    car.draw(ctx);
+    ctx.save();
+    ctx.translate(0, -bestCar.y + canvas.height * .8)
+
+    road.draw(ctx);
+    bestCar.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
 
