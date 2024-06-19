da169868b7b2f3ade80170bd328d7a1b02212a3b
Nenad Jakovchevski <89221061@student.upr.si>
2024-06-19 20:10:51 +0200

    Implement it v1.9

--- src/modules/car.ts
+++ src/modules/car.ts
@@ -66,12 +66,12 @@ class Car {
             const outputs = NeuralNetwork.feedForward(offsets, this.brain)
 
             if (this.useAI) {
-                this.controls.forward = outputs[0]
-                this.controls.left = outputs[1]
-                this.controls.right = outputs[2]
-                this.controls.reverse = outputs[3]
-            }
-
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+    
+                // Implement ray logic here
         }
     }
 
